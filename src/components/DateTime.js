7d7971bb5dbaa7457811d405efc69ab0559e9575
david-academy <david.andberg@gmail.com>
2019-08-19 16:53:44 +0300

    new plan adattu ja kommentoitu datetime.js

---
+++ src/components/DateTime.js
@@ -0,0 +1,26 @@
+// Tämä ei vaan toimii..
+
+// import React, { Fragment, useState } from "react";
+// import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+//
+//
+//  function BasicDateTimePicker() {
+//     const [selectedDate, handleDateChange] = useState(new Date());
+//
+//     return (
+//
+//         <MuiPickersUtilsProvider>
+//             <DateTimePicker
+//                 autoOk
+//                 ampm={false}
+//                 disableFuture
+//                 value={selectedDate}
+//                 onChange={handleDateChange}
+//                 label="24h clock"
+//             />
+//         </MuiPickersUtilsProvider>
+//
+//     );
+// }
+//
+//  export default BasicDateTimePicker;
